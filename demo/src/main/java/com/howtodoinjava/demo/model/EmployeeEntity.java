57c2f4ff96342b990a708c9a2a67aabd37171ebe
Suraj-Suryawanshi <74811909+Suraj-Suryawanshi@users.noreply.github.com>
2024-01-09 02:30:07 +0530

    Add files via upload

---
+++ demo/src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
@@ -0,0 +1,88 @@
+package com.howtodoinjava.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_EMPLOYEES")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name="task_name")
+    private String taskName;
+    
+    @Column(name="task_description")
+    private String taskDescription;
+    
+    @Column(name="priority")
+    private String priority;
+    
+
+    @Column(name="due_date")
+    private String dueDate;
+
+    @Column(name="status")
+    private String status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTaskName() {
+		return taskName;
+	}
+
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(String dueDate) {
+		this.dueDate = dueDate;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	@Override
+	public String toString() {
+		return "EmployeeEntity [id=" + id + ", taskName=" + taskName + ", taskDescription=" + taskDescription
+				+ ", priority=" + priority + ", dueDate=" + dueDate + ", status=" + status + "]";
+	}
+}
+ 
---
+++ demo/target/classes/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO 
+	TBL_EMPLOYEES (task_name, task_description, priority, due_date, status ) 
+VALUES
+  	('Lokesh',   '99 street los angles',  'Mumbai', 'Maharashtra','Completed'),
+  	('John',   '99 street los angles',  'Mumbai', 'Maharashtra', 'xyz@email.com');
